afd440c7d23bf63c2ca81a56cb6bbfe21a23c3ab
Sergey <sergey.naydenov48@gmail.com>
2021-10-23 16:39:22 +0300

    extract function

--- calc1.c
+++ calc1.c
@@ -22,44 +22,12 @@ void push(int s[], int * p, int v, int size)
     *p = *p + 1;    
 }
 
-int main()
-{   
-    //char c[5] = {'3', '5', '*', '2', '+'};
-    //char c[] = {'3', '5', '*', '2', '+'};
-    char c[] = "37+";
-    /*
-    c[0] = '3';
-    c[1] = '5';
-    c[2] = '*';
-    c[3] = '2';
-    c[4] = '+';
-    */
+int calculator(char c[]){
+    int i;
     int s[5];
     int sp = 0;
-  
-    int * SPadress;
-    SPadress = &sp;
-    /* *SPadress = 2; */
-     
-    int i;
-    int n = sizeof(c) - 1;
-    for (i = 0; i < n; ++i){
-        printf("%c\n", c[i]);
-    } 
-
-    /* 
-    // push(100)
-    s[sp] = 100;
-    sp = sp + 1;
     
-    // x = pop()
-    sp = sp - 1
-    x = s[sp];
-    */
-    
-    printf("Hello, World\n");
-    
-    for (i = 0; i < n; ++i){
+    for (i = 0; c[i] != 0; ++i){
         if (c[i] >= '0' and c[i] <= '9') {
             push(s, &sp, c[i] - 48, sizeof(s)/sizeof(s[0])); 
             printf("%d\n", s[sp-1]);
@@ -92,7 +60,44 @@ int main()
             push(s, &sp, r, sizeof(s)/sizeof(s[0]));
         }
     }
-    printf("result=%d\nsp=%d\n", s[sp - 1], sp);
+    
+    return s[sp-1];
+}    
+
+int main()
+{   
+    //char c[5] = {'3', '5', '*', '2', '+'};
+    //char c[] = {'3', '5', '*', '2', '+'};
+    char c[] = "37+";
+    /*
+    c[0] = '3';
+    c[1] = '5';
+    c[2] = '*';
+    c[3] = '2';
+    c[4] = '+';
+    */
+    
+    int spp;
+    int i;
+    int n = sizeof(c) - 1;
+    for (i = 0; i < n; ++i){
+        printf("%c\n", c[i]);
+    } 
+
+    /* 
+    // push(100)
+    s[sp] = 100;
+    sp = sp + 1;
+    
+    // x = pop()
+    sp = sp - 1
+    x = s[sp];
+    */
+    
+    printf("Hello, World\n");
+    
+    spp = calculator(c);
+    printf("result = %d\n", spp);
     return 0;
 }
 
