0f3c05240df3cdadb4395db03e90a950ef49ff45
Sergey <sergey.naydenov48@gmail.com>
2021-11-20 14:11:20 +0300

    introduce  stream of words

--- calc1.c
+++ calc1.c
@@ -31,6 +31,37 @@ struct Calcres {
 	int result;
 };
 
+struct word {
+    int nind;
+    char operation;
+    int number;
+};
+
+struct word_grinder {
+    char *d;
+    int i;
+};
+
+struct word get_word(struct word_grinder* wg)
+{
+    char c = wg->d[wg->i];
+    struct word r;
+    if (0 <= c - 48 && c - 48 <= 9){
+        r.nind = 1;
+        r.number = c - 48;
+        wg->i ++;
+    }
+    else if ( c == '+' || c == '-' ||
+              c == '*' || c == '/' ){
+         r.nind = 0;
+         r.operation = c;
+         wg->i ++;
+    }
+    else {
+        r.nind = -1;
+    }
+    return r;
+}
 
 struct Calcres calculator(char c[]){
     int i;
@@ -38,35 +69,33 @@ struct Calcres calculator(char c[]){
     //int sp = 0;
     struct Stack st;
     st.sp = 0;
-    
-    for (i = 0; c[i] != 0; ++i){
-        if (c[i] >= '0' and c[i] <= '9') {
-            st_push(&st, c[i] - 48, sizeof(st.s)/sizeof(st.s[0]));
+    struct word_grinder wg;
+    wg.i = 0;
+    wg.d = c;
+    while (1) {
+        struct word w;
+        w = get_word(&wg);
+        if (w.nind == -1){
+            break;
+        }
+        if (w.nind == 1){
+            st_push(&st, w.number, sizeof(st.s)/sizeof(st.s[0]));
             printf("%d\n", st.s[st.sp-1]);
         }
-        if (c[i] == '*' or c[i] == '+' or c[i] == '/' or c[i] == '-'){
-            //x = pop()
+        if (w.nind == 0){
             int r;
-/*
-            sp = sp - 1;
-            if (sp < 0) {
-                printf("Error: empty stack.\n");
-                exit(1);
-            }
-            int m2 = s[sp];
-*/
             int m2 = st_pop(&st);
             int m1 = st_pop(&st);
-            if (c[i] == '*') {
+            if (w.operation == '*') {
                 r = m1 * m2;
             }
-            else if (c[i] == '+') {
+            else if (w.operation == '+') {
                 r = m1 + m2;
             }
-            else if (c[i] == '/') {
+            else if (w.operation == '/') {
                 r = m1 / m2;
             }
-            else if (c[i] == '-') {
+            else if (w.operation == '-') {
                 r = m1 - m2;    
             }
             st_push(&st, r, sizeof(st.s)/sizeof(st.s[0]));
